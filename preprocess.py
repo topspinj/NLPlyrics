ffed95bd81174eb2a3a7e4f8bb657209a9021053
Jill Cates <cates.jill@gmail.com>
2018-09-11 23:33:36 -0400

    Move tfidf vectorization to modeling phase

--- preprocess.py
+++ preprocess.py
@@ -5,19 +5,17 @@ This script performs data cleaning and pre-processing
 on the song lyrics dataset. The following steps are covered:
 - tokenizes lyrics
 - lemmatizes words of song lyrics
-- creates a TFIDF sparse csr matrix for topic modeling
+- re-concatenates words for TFIDF vectorization
+- writes updated dataframe to a csv file
 
 To execute this script, run the follow command in your terminal:
 ```
-python preprocess.py --genre=hiphop --save_csv (optional)
+python preprocess.py --genre=hiphop (optional)
 ```
 Due to the large nature of this dataset, lyrics are processed
 by genre. Indicate which genre to process using the `genre` arg.
 
-The script automatically writes the TFIDF matrix to a .npz file.
-However, if you would like to also save the updated dataframe with
-additional column `tokenized_lyrics`, then add the `save_csv` flag
-to the command above. 
+Note: The script automatically writes the updated dataframe to a .csv file.
 """
 import os
 import sys
@@ -65,8 +63,7 @@ def lemmatize_lyrics(tokens):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Data cleaning and feature engineering")
-    parser.add_argument('--save_csv', dest='save', default=False, action='store_true')
+        description="Song lyric cleaning and pre-processing")
     parser.add_argument('--genre', type=str, dest='genre', default='hiphop')
 
     args = parser.parse_args()
@@ -84,7 +81,6 @@ if __name__ == "__main__":
 
     print("Tokenizing lyrics for all songs of genre {:s}...".format(genre))
     lyrics['tokenized_lyrics'] = lyrics['lyrics'].apply(tokenize_lyrics)
-    print(lyrics['tokenized_lyrics'][249])
 
     print("Calculating word count...")
     lyrics['word_count'] = lyrics['tokenized_lyrics'].apply(len)
@@ -93,15 +89,8 @@ if __name__ == "__main__":
     lyrics['tokenized_lyrics'] = lyrics['tokenized_lyrics'].apply(lemmatize_lyrics)
 
     print("Concatenating lyrics...")
-    processed_lyrics = lyrics['tokenized_lyrics'].apply(lambda x: ' '.join(x))
+    lyrics['processed_lyrics'] = lyrics['tokenized_lyrics'].apply(lambda x: ' '.join(x))
 
-    print("Performing TFIDF vectorization...")
-    tfidf = TfidfVectorizer()
-    X = tfidf.fit_transform(processed_lyrics)
-    save_npz('data/tfidf_{:s}.npz'.format(args.genre), X)
-    print("TFIDF matrix saved! Check data/ directory.")
-
-    if args.save is True:
-        path = "data/lyrics_{:s}.csv".format(args.genre)
-        lyrics.to_csv(path)
-        print("CSV file saved! Check path: {:s}".format(path))
+    path = "data/lyrics_{:s}.csv".format(args.genre.lower())
+    lyrics.to_csv(path)
+    print("CSV file saved! Check path: {:s}".format(path))
